86130985f9a8e5b78677beca19c5ae9e272eb6b2
Fenykepy <phiroom@lavilotte-rolle.fr>
2015-01-26 18:53:53 +0100

    end of base reactions module refactoring.

--- app/js/controllers/baseReactionsCtrl.js
+++ app/js/controllers/baseReactionsCtrl.js
@@ -8,18 +8,17 @@ rdmControllers.controller('baseReactionsCtrl', ['$scope', 'baseReactionsSrv',
         function($scope, baseReactionsSrv) {
 
         $scope.reac = baseReactionsSrv;
-        $scope.force = baseReactionsSrv.force_group;
         $scope.$parent.sub_title = baseReactionsSrv.title;
 
         $scope.$watch('reac.ab', baseReactionsSrv.refreshAll);
         $scope.$watch('reac.a_type', baseReactionsSrv.refreshAll);
         $scope.$watch('reac.b_type', baseReactionsSrv.refreshAll);
         
-        $scope.$watch('force.f.value', baseReactionsSrv.refreshForm);
-        $scope.$watch('force.f.angle', baseReactionsSrv.refreshForm);
-        $scope.$watch('force.f.hdir', baseReactionsSrv.refreshForm);
-        $scope.$watch('force.f.vdir', baseReactionsSrv.refreshForm);
-        $scope.$watch('force.afh', baseReactionsSrv.refreshForm);
-        $scope.$watch('force.afv', baseReactionsSrv.refreshForm);
+        $scope.$watch('reac.force_group.f.value', baseReactionsSrv.refreshForm);
+        $scope.$watch('reac.force_group.f.angle', baseReactionsSrv.refreshForm);
+        $scope.$watch('reac.force_group.f.hdir', baseReactionsSrv.refreshForm);
+        $scope.$watch('reac.force_group.f.vdir', baseReactionsSrv.refreshForm);
+        $scope.$watch('reac.force_group.afh', baseReactionsSrv.refreshForm);
+        $scope.$watch('reac.force_group.afv', baseReactionsSrv.refreshForm);
         
 }]);
